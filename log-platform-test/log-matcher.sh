4099dde16bc6985df7eebf652525fc2125f940c3
Vivek Juneja <vivekjuneja@gmail.com>
2017-01-12 16:54:08 +0900

    remove sample logs, changed logstash and filebeat config

--- log-platform-test/log-matcher.sh
+++ log-platform-test/log-matcher.sh
@@ -12,32 +12,30 @@ SORT_FIELD_ASC="asc"
 SORT_FIELD_DESC="desc"
 MAX_LOGS="10000"
 LOG_INDEX_PREF="logstash"
-RAW_LOG_SOURCE="remote"
+RAW_LOG_SOURCE="local"
 RAW_LOG_USER=""
 RAW_LOG_HOST=""
-LOG_FILTER=""
-ELKTAIL_PATH=""
+ELKTAIL_PATH="./elktail"
 ELASTICSEARCH_PROTO="http"
-ELASTICSEARCH_HOST=""
-ELASTICSEARCH_PORT=""
-SORT_FIELD=""
-SORT_FIELD_ASC=""
-LOG_FIELD=""
+ELASTICSEARCH_HOST="192.168.99.101"
+ELASTICSEARCH_PORT="9200"
+SORT_FIELD="kafka.offset"
+LOG_FIELD="data"
 MAX_LOGS="10000"
-LOG_INDEX_PREF="logstash"
-LOG_INDEX_DATE=""
-LOG_FILTER_1=""
-LOG_FILTER_2=""
-RAW_LOG_PATH=""
-PERSIST_LOGS=""
+LOG_INDEX_DATE="$1"
+LOG_FILTER_1="$2"
+LOG_FILTER_2="_ordCreLog"
+RAW_LOG_PATH="$3"
+PERSIST_LOGS="$4"
 PERSIST_LOGS_PERSIST="persist"
+
 
 
 if [ "x$RAW_LOG_SOURCE" == "xremote" ];then
 	echo "checking for raw logs on the remote server"
 	
 	# Get raw logs from remote server and filter the log filter and given pattern for processing
-	ssh $RAW_LOG_USER@$RAW_LOG_HOST cat $RAW_LOG_PATH | grep "$LOG_FILTER_1" |  grep "$LOG_FILTER_2" > ${LOG_FILTER_1}_source_processed.log
+	ssh $RAW_LOG_USER@$RAW_LOG_HOST cat $RAW_LOG_PATH | grep "$LOG_FILTER_1" > ${LOG_FILTER_1}_source_processed.log
 else
 	echo "checking for raw logs on the local file system path"
 
@@ -45,10 +43,16 @@ else
 	cat $RAW_LOG_PATH | grep "$LOG_FILTER_1" | grep "$LOG_FILTER_2" > ${LOG_FILTER_1}_source_processed.log
 fi
 
+
+
+
+echo "checking from Elasticsearch"
 # Get logs from the Elasticsearch 
+
 $ELKTAIL_PATH --url "$ELASTICSEARCH_PROTO://$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT" \
 -o "$SORT_FIELD" -r "$SORT_FIELD_ASC" -f "%$LOG_FIELD" -l -n "$MAX_LOGS" \
--i "$LOG_INDEX_PREF-$LOG_INDEX_DATE" "$LOG_FILTER_1" AND "$LOG_FILTER_2"  > ${LOG_FILTER_1}_elk.log
+-i "$LOG_INDEX_PREF-$LOG_INDEX_DATE" "$LOG_FILTER_1" AND "$LOG_FILTER_2" > ${LOG_FILTER_1}_elk.log
+
 
 # Process the raw log, by filtering the LOG_FILTER, same as the Raw logs
 cat ${LOG_FILTER_1}_elk.log | grep "$LOG_FILTER_2" > ${LOG_FILTER_1}_elk_processed.log
